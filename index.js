53eeba7e162f010dd7bd9956b717db4f75acd71c
chenty1 <chenty1@guahao.com>
2020-09-19 22:56:44 +0800

    feat: valueOf+toString

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ require('fs').createReadStream('./tsconfig.json', {
 }).on('data', function (data) {
   console.log(data)
 })
+
+var obj = {
+  toString:function(){
+    console.log('toString')
+    return Object.prototype.toString.call(this)
+  },
+  valueOf:function(){
+    console.log('valueOf')
+    return Object.prototype.valueOf.call(this)
+  }
+}
+console.log(obj)
+console.log(+obj) // 先触发valueOf，再触发toString，得到NaN
+console.log(obj=={})
+console.log(obj==={})
+console.log(obj=='test') // 先触发valueOf，再触发toString，得到false
+console.log(obj==='test')
